60f0fa357601df932098df96a37905e6f116f311
Levi-Magny <lemmagny@gmail.com>
2021-09-06 01:41:57 -0300

    feat: Interactive header

--- netflix_clone/src/components/Header/component/Header.js
+++ netflix_clone/src/components/Header/component/Header.js
@@ -6,12 +6,12 @@ function Header({black}){
         <header className={black ? 'black' : ''}>
             <div className="header--logo">
                 <a href="/">
-                    <img src="https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-5.png"/>
+                    <img alt="logo" src="https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-5.png"/>
                 </a>
             </div>
             <div className="header--user">
                 <a href="/">
-                    <img alt="Usuário" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"/>
+                    <img alt="Usuario" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"/>
                 </a>
             </div>
         </header>
